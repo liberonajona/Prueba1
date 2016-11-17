1ccfe96e8927ff58655113c5e88ec9f25b7cb317
Liberona <Liberona@DESKTOP-PDJ62EU.inacap.cl>
2016-11-17 14:06:09 -0300

    V-0.1

---
+++ PruebaNro1/src/pruebanro1/PruebaNro1.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebanro1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Liberona
+ */
+public class PruebaNro1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ingresa un numero");
+        int num1 = sc.nextInt();
+        System.out.println("ingresa segundo numero");
+        int num2 = sc.nextInt();
+        int suma = num1 + num2;
+        System.out.println("la suma es : "+suma);
+        
+        
+    }
+    
+}
